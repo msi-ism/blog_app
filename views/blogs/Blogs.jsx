941f39057f8ef52e84b118e59c563fcead462b2b
msi-ism <msism720@gmail.com>
2022-11-07 09:02:32 -0500

    ui updates

--- views/blogs/Blogs.jsx
+++ views/blogs/Blogs.jsx
@@ -12,18 +12,18 @@ class Blogs extends React.Component {
         </head>
         <Navbar loggedInUser={loggedInUser} />
 
-        <h1 style ={styles.header}>Blog Posts</h1>
-        <a style ={styles.createPostLink} href='/blog/new'>Write a new note!</a>
-        <div className='browse-container'>
-          <ul style={styles.timeline} className='browse-container'>
+        <h1>Blog Posts</h1>
+        <a href='/blog/new' className='create-post-link'>Write a new note!</a>
+        <div >
+          <ul  className='browse-container'>
             {blogs.map((blog, idx) => (
-              <li style={styles.item} key={idx} className='post-it'>
+              <li  key={idx} className='post-it'>
                 {" "}
+                <p className="post-it-username">{'@'+ blog.author}</p>
                 <a href={`blog/${blog._id}`}><h2>{blog.title}</h2></a>
-                <p>{blog.body}</p>
-                <p style={styles.likes}>&hearts; {blog.likes}</p>
-                <p>{'@'+ blog.author}</p>
-                {blog.author === loggedInUser ? (<a style={styles.createPostLink} href={`blog/${blog._id}/edit`}>Edit Post!</a>) : null }
+                <p className='note-body'>{blog.body}</p>
+                <p>&hearts; {blog.likes}</p>
+                {blog.author === loggedInUser ? (<a  href={`blog/${blog._id}/edit`}>Edit Post!</a>) : null }
                 {blog.author === loggedInUser ? (<form action={`/blog/${blog._id}?_method=DELETE`} method='POST'>
                     <input type='submit' value='Delete post!' />
                 </form>) : null }
@@ -36,53 +36,51 @@ class Blogs extends React.Component {
   }
 }
 
-const styles = {
-  container: {
-    backgroundColor: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  timeline: {
-    margin: '20px',
-    padding: '40px'
-  },
-  header: {
-    fontSize: '32px',
-    color: 'black',
+// const styles = {
+//   container: {
+//     backgroundColor: 'white',
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center'
+//   },
+//   timeline: {
+//     margin: '20px',
+//     padding: '40px'
+//   },
+//   header: {
+//     fontSize: '32px',
+//     color: 'black',
 
-  },
-  ulContainer: {
-    backgroundColor: '#fff',
-    padding: '15px',
-    flexDirection: 'reverse-column'
-  },
-  createPostLink: {
-    backgroundColor: '#fff',
-    padding: '5px',
-    borderRadius: '5px',
-    textDecoration: 'none',
-    marginLeft: '20px',
-    marginBottom: '20px'
-  },
-  item: {
-    backgroundColor: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    padding: '10px',
-    listStyleType: 'none',
-    border: '2px solid rgba(0, 0, 150, 0.3)',
-    margin: '10px',
-    width: '100%'
-  },
-  likes: {
-    display: 'inline-block',
-    alignSelf: 'right',
-    color: 'red',
-    float: 'right',
+//   },
+//   ulContainer: {
+//     backgroundColor: '#fff',
+//     padding: '15px',
+//     flexDirection: 'reverse-column'
+//   },
+//   createPostLink: {
+//     backgroundColor: '#fff',
+//     padding: '5px',
+//     borderRadius: '5px',
+//     textDecoration: 'none',
+//     marginLeft: '20px',
+//     marginBottom: '20px'
+//   },
+//   item: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     listStyleType: 'none',
+//     border: '2px solid rgba(0, 0, 150, 0.3)',
+//     margin: '10px',
+//     width: '100%'
+//   },
+//   likes: {
+//     display: 'inline-block',
+//     alignSelf: 'right',
+//     color: 'red',
+//     float: 'right',
 
-  }
-}
+//   }
+// }
 
 module.exports = Blogs
 
