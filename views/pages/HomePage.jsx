3fc00adc13359bfc61fbf674accad53b2f1f2bbf
msi-ism <msism720@gmail.com>
2022-12-30 17:59:59 -0500

    dummy home note added

--- views/pages/HomePage.jsx
+++ views/pages/HomePage.jsx
@@ -2,12 +2,6 @@ const React = require("react");
 const ReactDom = require("react-dom");
 const Navbar = require("../components/Navbar");
 
-let fillerText = "What's on your mind?";
-
-let clearFillerText = () => {
-  fillerText = "";
-};
-
 class HomePage extends React.Component {
   render() {
     const { loggedInUser } = this.props;
@@ -21,24 +15,43 @@ class HomePage extends React.Component {
           <Navbar loggedInUser={loggedInUser} />
         </header>
         <div className="home-container">
-          {/* <img className='header-img' src='images/AYA2022wText.png'></img> */}
           <h1>Notes to Self</h1>
-          {/* <h3>Discovering ourselves, together.</h3> */}
           <p>
             <span className="question-txt">What's something that you need to be reminded of?</span>
           </p>
-          {/* <textarea placeholder='How are you feeling?'></textarea> */}
-          <div class="post-it">
-            <h2 className="post-it-title">Note to Self:</h2>
-            {/* <ul>
-                            <li className='input-box' contentEditable='true' onClick={clearFillerText}>{fillerText}</li>
-                        </ul> */}
-            <textarea
-              className="input-box"
-              type="text"
-              placeholder="What's on your mind?"
-            ></textarea>
-          </div>
+          <div className="post-it">
+                {" "}
+                <div className="post-it-top">
+                  <a>
+                    <h2 className="post-it-title">Alone Time</h2>
+                  </a>
+                </div>
+                <p className="post-it-body">Sometimes I feel like I'm all alone in this world.</p>
+                <div className="post-it-bottom">
+                  <p className="post-it-username">{"@" + "marySue"}</p>
+                  <div className='user-icos'>
+                  <p className="post-it-likes">&hearts; 9</p>
+                    <a className="post-it-edit">
+                      <img src="/images/edit.png"></img>
+                    </a>
+                    <form
+                      className="post-it-delete"
+                    >
+                      <div className="delete-container">
+                        <input
+                          className="delete-input"
+                          type="submit"
+                          value=""
+                        ></input>
+                        <img
+                          className="delete-img"
+                          src="/images/trash.png"
+                        ></img>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
         </div>
       </div>
     );
