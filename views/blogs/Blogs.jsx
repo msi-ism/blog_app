bf0cbe589e4702f6336e39b9b2220f27d751e229
msi-ism <msism720@gmail.com>
2023-01-16 13:34:01 -0500

    added bottom margin hack to fix home page gap

--- views/blogs/Blogs.jsx
+++ views/blogs/Blogs.jsx
@@ -5,63 +5,68 @@ class Blogs extends React.Component {
   render() {
     const { blogs, loggedInUser } = this.props;
     console.log("blogs" + loggedInUser);
-    console.log(loggedInUser)
+    console.log(loggedInUser);
     return (
-      <div className="main-container">
+      <div>
         <head>
           <link rel="stylesheet" href="/CSS/app.css"></link>
         </head>
         <Navbar loggedInUser={loggedInUser} />
-        <div className="create-post-div">
-          <h1 className="page-title">Bulletin Board</h1>
-          <a href="/blog/new" className="create-post-link">
-            Write a new note!
-          </a>
-        </div>
-        <div className="bulletin-board">
-          <ul className="browse-container">
-            {blogs.map((blog, idx) => (
-              <li key={idx} className="post-it">
-                {" "}
-                <div className="post-it-top">
-                  <a href={`blog/${blog._id}`}>
-                    <h2 className="post-it-title">{blog.title}</h2>
-                  </a>
-                </div>
-                <p className="post-it-body">{blog.body}</p>
-                <div className="post-it-bottom">
-                  <p className="post-it-username">{"@" + blog.author}</p>
-                  <div className='user-icos'>
-                  {/* <p className="post-it-likes">&hearts; {blog.likes}</p> */}
-                  {blog.author === loggedInUser ? (
-                    <a className="post-it-edit" href={`blog/${blog._id}/edit`}>
-                      <img src="/images/edit.png"></img>
+        <div className="main-container">
+          <div className="create-post-div">
+            <h1 className="page-title">Bulletin Board</h1>
+            <a href="/blog/new" className="create-post-link">
+              Write a new note!
+            </a>
+          </div>
+          <div className="bulletin-board">
+            <ul className="browse-container">
+              {blogs.map((blog, idx) => (
+                <li key={idx} className="post-it">
+                  {" "}
+                  <div className="post-it-top">
+                    <a href={`blog/${blog._id}`}>
+                      <h2 className="post-it-title">{blog.title}</h2>
                     </a>
-                  ) : null}
-                  {blog.author === loggedInUser ? (
-                    <form
-                      className="post-it-delete"
-                      action={`/blog/${blog._id}?_method=DELETE`}
-                      method="POST"
-                    >
-                      <div className="delete-container">
-                        <input
-                          className="delete-input"
-                          type="submit"
-                          value=""
-                        ></input>
-                        <img
-                          className="delete-img"
-                          src="/images/trash.png"
-                        ></img>
-                      </div>
-                    </form>
-                  ) : null}
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                  <p className="post-it-body">{blog.body}</p>
+                  <div className="post-it-bottom">
+                    <p className="post-it-username">{"@" + blog.author}</p>
+                    <div className="user-icos">
+                      {/* <p className="post-it-likes">&hearts; {blog.likes}</p> */}
+                      {blog.author === loggedInUser ? (
+                        <a
+                          className="post-it-edit"
+                          href={`blog/${blog._id}/edit`}
+                        >
+                          <img src="/images/edit.png"></img>
+                        </a>
+                      ) : null}
+                      {blog.author === loggedInUser ? (
+                        <form
+                          className="post-it-delete"
+                          action={`/blog/${blog._id}?_method=DELETE`}
+                          method="POST"
+                        >
+                          <div className="delete-container">
+                            <input
+                              className="delete-input"
+                              type="submit"
+                              value=""
+                            ></input>
+                            <img
+                              className="delete-img"
+                              src="/images/trash.png"
+                            ></img>
+                          </div>
+                        </form>
+                      ) : null}
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     );
